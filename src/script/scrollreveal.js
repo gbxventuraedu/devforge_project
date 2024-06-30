d67165d6157be5697e3bb514df376bb21c5b4c68
Gabriel Ventura <89049503+gbxventura@users.noreply.github.com>
2024-06-30 16:56:06 -0300

    FINALIZANDO

---
+++ src/script/scrollreveal.js
@@ -0,0 +1,41 @@
+ScrollReveal().reveal('.elemento', {
+
+    duration: 1000,
+    origin: 'left',
+    distance: '50px',
+    reset: true
+  })
+  const scrollRevealOption = {
+    distance: "50px",
+    origin: "left",
+    duration: 1000,
+  }
+  ScrollReveal().reveal(".elemento2", {
+    ...scrollRevealOption,
+    delay: 500,
+  })
+  ScrollReveal().reveal(".elemento3", {
+    ...scrollRevealOption,
+    delay: 1000,
+  })
+  ScrollReveal().reveal(".elemento4", {
+    ...scrollRevealOption,
+    delay: 1500,
+  })
+  ScrollReveal().reveal(".elemento5", {
+    ...scrollRevealOption,
+    delay: 2000,
+  })
+  ScrollReveal().reveal(".elemento6", {
+    ...scrollRevealOption,
+    delay: 2500,
+  })
+  ScrollReveal().reveal(".elemento7", {
+    ...scrollRevealOption,
+    delay: 3000,
+  })
+  ScrollReveal().reveal(".elementox", {
+    ...scrollRevealOption,
+    delay: 1000,
+    origin: "bottom",
+  })
