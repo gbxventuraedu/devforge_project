5cfe3c60251c36273645ddde81f6f1dfc27dccaa
Gabriel Ventura <89049503+gbxventura@users.noreply.github.com>
2024-07-01 11:28:40 -0300

    atualizações referente ao efeito, dimuição de delay e remoção do reset true

--- src/script/scrollreveal.js
+++ src/script/scrollreveal.js
@@ -1,41 +1,40 @@
 ScrollReveal().reveal('.elemento', {
-
-    duration: 1000,
-    origin: 'left',
-    distance: '50px',
-    reset: true
-  })
-  const scrollRevealOption = {
-    distance: "50px",
-    origin: "left",
-    duration: 1000,
-  }
-  ScrollReveal().reveal(".elemento2", {
-    ...scrollRevealOption,
-    delay: 500,
-  })
-  ScrollReveal().reveal(".elemento3", {
-    ...scrollRevealOption,
-    delay: 1000,
-  })
-  ScrollReveal().reveal(".elemento4", {
-    ...scrollRevealOption,
-    delay: 1500,
-  })
-  ScrollReveal().reveal(".elemento5", {
-    ...scrollRevealOption,
-    delay: 2000,
-  })
-  ScrollReveal().reveal(".elemento6", {
-    ...scrollRevealOption,
-    delay: 2500,
-  })
-  ScrollReveal().reveal(".elemento7", {
-    ...scrollRevealOption,
-    delay: 3000,
-  })
-  ScrollReveal().reveal(".elementox", {
-    ...scrollRevealOption,
-    delay: 1000,
-    origin: "bottom",
-  })
+  duration: 1000,
+  origin: 'left',
+  distance: '50px',
+  reset: false,
+});
+const scrollRevealOption = {
+  distance: '50px',
+  origin: 'left',
+  duration: 1000,
+};
+ScrollReveal().reveal('.elemento2', {
+  ...scrollRevealOption,
+  delay: 1000,
+});
+ScrollReveal().reveal('.elemento3', {
+  ...scrollRevealOption,
+  delay: 1150,
+});
+ScrollReveal().reveal('.elemento4', {
+  ...scrollRevealOption,
+  delay: 1300,
+});
+ScrollReveal().reveal('.elemento5', {
+  ...scrollRevealOption,
+  delay: 1450,
+});
+ScrollReveal().reveal('.elemento6', {
+  ...scrollRevealOption,
+  delay: 1600,
+});
+ScrollReveal().reveal('.elemento7', {
+  ...scrollRevealOption,
+  delay: 1850,
+});
+ScrollReveal().reveal('.elementox', {
+  ...scrollRevealOption,
+  delay: 2000,
+  origin: 'bottom',
+});
